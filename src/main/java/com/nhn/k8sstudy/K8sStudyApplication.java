e1d584f78e27c9a8cee49a89522164a8457ebe5f
김성현/인프라플랫폼개발팀/CL <sunghyun.kim@nhn.com>
2023-06-30 14:08:45 +0900

    project init

---
+++ src/main/java/com/nhn/k8sstudy/K8sStudyApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.k8sstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhn/k8sstudy/K8sStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.k8sstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
